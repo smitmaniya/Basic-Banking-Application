410ec96e1926e60b5fba7f5811303dad7e71d742
smitmaniya <smitmaniya5@gmail.com>
2021-05-21 12:50:11 +0530

    This is the First Commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,109 @@
+import 'package:basic_bank_app/model/customer.dart';
+import 'package:basic_bank_app/screens/adminDetails.dart';
+import 'package:basic_bank_app/screens/customers_list.dart';
+import 'package:basic_bank_app/screens/insertCustomer.dart';
+import 'package:basic_bank_app/widgets/customcard.dart';
+import 'package:flutter/material.dart';
+import '../dbHelper.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Customer> query = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('Co-Operative Bank'),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.info),
+              onPressed: () async {
+                DBHelper db = DBHelper();
+                query = await db.getCustomer();
+                Navigator.of(context).pushNamed(AdminDetailsScreen.routeNamed,
+                    arguments: query[0]);
+              })
+        ],
+      ),
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+                elevation: 10,
+                margin: EdgeInsets.all(3),
+                color: Colors.orange[100],
+                child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60),
+                    ),
+                    child: Image(
+                      image: AssetImage("assets/images/logo.png"),
+                      height: 250,
+                      width: 250,
+                      fit: BoxFit.fill,
+                      color: Colors.orangeAccent,
+
+                    )),
+              ),
+              const Divider(
+                color: Colors.orange,
+                height: 20,
+                thickness: 5,
+                endIndent: 0,
+              ),
+              Text(
+                "Welcome! To The Co-operative Bank ",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+              const Divider(
+                color: Colors.orange,
+                height: 20,
+                thickness: 5,
+                endIndent: 0,
+              ),
+              SizedBox(height: 50),
+              CustomCard(
+                icon: Icons.account_box_outlined,
+                title: 'View All Customers List',
+                navpage: () async {
+                  DBHelper db = DBHelper();
+                  query = await db.getCustomer();
+                  Navigator.of(context)
+                      .pushNamed(CustomersList.routeNamed, arguments: query);
+                },
+              ),
+              CustomCard(
+                icon: Icons.account_balance_outlined,
+                title: 'Insert Customer',
+                navpage: () {
+                  Navigator.of(context).pushNamed(
+                    InsertCustomer.routeNamed,
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/customcard.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Function navpage;
+
+  CustomCard({this.icon, this.title, this.navpage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Card(
+        color: Colors.orange,
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(10),
+                topRight: Radius.circular(10)),
+            side: BorderSide(width: 8, color: Colors.black)),
+
+        child: ListTile(
+          contentPadding: EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
+          leading: Icon(
+            icon,
+            color: Colors.white,
+            size: 60,
+          ),
+          title: Text(
+            title,
+            style: TextStyle(
+              fontSize: 24,
+              color: Colors.white,
+            ),
+          ),
+          onTap: navpage,
+        ),
+      ),
+    );
+  }
+}
